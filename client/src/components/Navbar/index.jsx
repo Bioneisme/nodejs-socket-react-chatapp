8035d6de81c5d72c96524d8eede1fdf3f940981a
Bioneisme <bioneisme@gmail.com>
2022-07-18 17:00:28 +0600

    update

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -3,40 +3,18 @@ import "./navbar.css"
 import {clearState, logoutUser} from "../../store/slices/userSlice";
 import {useDispatch} from 'react-redux'
 
-const Index = ({email}) => {
+
+const Index = () => {
     const dispatch = useDispatch();
 
     const logout = () => {
         dispatch(logoutUser());
         dispatch(clearState())
     };
-
     return (
-        <nav className="navbar navbar-light">
-            <div className="navbar-brand">
-                <a href="/" className="text mx-3">
-                    Home
-                </a>
-                <a href="/registration" className="text mx-3">
-                    Sign Up
-                </a>
-                <a href="/login" className="text mx-3">
-                    Log In
-                </a>
-            </div>
-            <div className="navbar-brand">
-                <div>
-                    <a href="/profile" style={{textDecoration: "none", color: "black"}}>
-                        <i className="fa fa-user-circle fa-lg m-2" aria-hidden="true"/>
-                        {email}
-                    </a>
-                    <a style={{cursor: "pointer"}}
-                       onClick={logout}>
-                        <i className="mx-2 fa fa-remove"/>
-                    </a>
-                </div>
-            </div>
-        </nav>
+        <div>
+
+        </div>
     );
 };
 
--- client/src/pages/Layout/index.jsx
+++ client/src/pages/Layout/index.jsx
@@ -1,27 +1,11 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import Index from "../../components/Navbar";
 import {Outlet} from "react-router-dom"
-import {useDispatch, useSelector} from 'react-redux';
-import {userSelector, getUser} from '../../store/slices/userSlice';
 
 const Layout = () => {
-    const {currentUser} = useSelector(userSelector)
-    const [email, setEmail] = useState(null)
-
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        if (currentUser) {
-            setEmail(currentUser.email);
-        }
-        dispatch(getUser())
-    }, [currentUser]);
-
     return (
         <>
-            <Index
-                email={email}
-            />
+            <Index/>
             <Outlet/>
         </>
     );
